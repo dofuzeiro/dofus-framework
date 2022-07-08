4284f4ace161153e71f827eab4c738476d577b21
Rafael Ribeiro <rafa456@outlook.pt>
2022-07-08 15:28:41 +0100

    fix(src/io/file/deserializer.rs): improved error throwing (it is more generic). Removed dupped code
    ref #28

--- src/io/file/deserializer.rs
+++ src/io/file/deserializer.rs
@@ -1,9 +1,8 @@
-use std::fs;
-
 use serde::Deserialize;
+use std::fs;
 use thiserror::Error;
 
-use crate::io::file::deserializer::DeserializationError::{InvalidInput, NotFound};
+use crate::io::file::deserializer::DeserializationError::{EmptyFile, IOError, InvalidInput};
 
 #[derive(Debug, Error)]
 pub enum DeserializationError {
@@ -11,12 +10,17 @@ pub enum DeserializationError {
     NotImplemented(String),
     #[error("Cannot deserialize data:\n\n{0}\n\nas {1} format")]
     InvalidInput(String, &'static str),
-    #[error("No file found at {0}")]
-    NotFound(String),
+    #[error("File {0} is empty")]
+    EmptyFile(String),
+    #[error("Error while trying to read the contents of the file")]
+    IOError {
+        path: String,
+        source: std::io::Error,
+    },
 }
 
 pub trait Deserializer {
-    fn deserialize<E>(&self, data: String) -> Result<E, DeserializationError>
+    fn deserialize<E>(&self, data: &str) -> Result<E, DeserializationError>
     where
         E: for<'a> Deserialize<'a>;
 }
@@ -30,26 +34,27 @@ pub enum Format {
 }
 
 impl Deserializer for Format {
-    fn deserialize<E>(&self, data: String) -> Result<E, DeserializationError>
+    fn deserialize<E>(&self, data: &str) -> Result<E, DeserializationError>
     where
         E: for<'a> Deserialize<'a>,
     {
-        let data = data.as_str();
         match self {
-            Format::Json => {
-                serde_json::from_str(data).map_err(|_| InvalidInput(data.to_string(), "JSON"))
-            }
-            Format::Yaml => {
-                serde_yaml::from_str(data).map_err(|_| InvalidInput(data.to_string(), "YAML"))
-            }
-            Format::Xml => {
-                serde_xml_rs::from_str(data).map_err(|_| InvalidInput(data.to_string(), "XML"))
-            }
-            Format::Toml => {
-                toml::from_str(data).map_err(|_| InvalidInput(data.to_string(), "TOML"))
-            } //_ => Err(NotImplemented(format!("{:?}", self))),
+            Format::Json => deserialize_with_context(|x| serde_json::from_str(x), data, "JSON"),
+            Format::Yaml => deserialize_with_context(serde_yaml::from_str, data, "YAML"),
+            Format::Xml => deserialize_with_context(serde_xml_rs::from_str, data, "XML"),
+            Format::Toml => deserialize_with_context(|x| toml::from_str(x), data, "TOML"),
+            //_ => Err(NotImplemented(format!("{:?}", self))),
         }
     }
+}
+
+fn deserialize_with_context<T, E>(
+    //    result: Result<T, E>,
+    result: fn(&str) -> Result<T, E>,
+    data: &str,
+    format: &'static str,
+) -> Result<T, DeserializationError> {
+    result(data).map_err(|_| InvalidInput(data.to_string(), format))
 }
 
 pub fn deserialize_from_file<E, T>(
@@ -61,6 +66,14 @@ where
     T: Deserializer,
 {
     fs::read_to_string(file_path)
-        .map_err(|_| NotFound(file_path.to_string()))
-        .map(|file_data| deserializer.deserialize(file_data))?
+        .map_err(|err| IOError {
+            path: file_path.to_string(),
+            source: err,
+        })
+        .map(|file_data| {
+            if file_data.is_empty() {
+                return Err(EmptyFile(file_path.to_string()));
+            }
+            deserializer.deserialize(file_data.as_str())
+        })?
 }
